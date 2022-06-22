b72db23a4545f94369da5c8b6f3cd9e89f879f31
Nicholas Hibbits <nicholashibbits@gmail.com>
2022-06-22 02:43:29 -0700

    scaffold NewQuestion component

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -1,5 +1,22 @@
+import "../styles/newQuestion.css";
+
 const NewQuestion = () => {
-  return <div className="new-question-component">New Question</div>;
+  return (
+    <div className="new-question-component">
+      <h2>Create Question</h2>
+      <div className="create-question-subheader">Would you rather...</div>
+      <section className="create-option-container">
+        <div className="create-option-wrapper">
+          <label htmlFor="option-one">Option 1</label>
+          <textarea id="option-one" placeholder="Enter text" />
+        </div>
+        <div className="create-option-wrapper">
+          <label htmlFor="option-two">Option 2</label>
+          <textarea id="option-two" placeholder="Enter text" />
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default NewQuestion;

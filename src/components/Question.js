c3669753257340a3e3fd0f4ab8c90a9296f4053a
Nicholas Hibbits <nicholashibbits@gmail.com>
2022-06-22 01:00:44 -0700

    style question cards

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,7 +1,12 @@
 import "../styles/_questions.css";
 
 const Question = ({ questionId }) => {
-  return <div className="question-component">{questionId}</div>;
+  return (
+    <div className="question-component">
+      <div className="card-title">User</div>
+      <div className="card-date">0:00pm, 00/00/0000</div>
+    </div>
+  );
 };
 
 export default Question;

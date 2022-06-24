c366f6645b6aa09295bc056444f7b72b6df7a1f2
Nicholas Hibbits <nicholashibbits@gmail.com>
2022-06-24 13:23:45 -0700

    start auth flow

---
+++ src/reducers/auth.js
@@ -0,0 +1,11 @@
+import { SET_AUTH } from "../actions/auth";
+
+export default function auth(state = "", action) {
+  switch (action.type) {
+    case SET_AUTH:
+      return {
+        ...state,
+        ...action.auth,
+      };
+  }
+}
